58ff78c35b57817ea150271b9421020d7210dcf8
CodeSShuv <codeshuv@gmail.com>
2024-02-08 01:21:29 +0545

    changed to es6 version of imp

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const leaderBoardData = require("./leaderBoard.json");
-const data = require("./data.json");
-const cors = require('cors');
-const fileSupport = require("fs");
+import express, { json } from "express";
+import leaderBoardData from "./leaderBoard.json"assert { type: "json" };
+import data from "./data.json" assert { type: "json" };
+import cors from 'cors';
+import { writeFile } from "fs";
 const app = express();
 const port = 5000;
 app.use(cors());
-app.use(express.json());
+app.use(json());
 app.get("/api/studentlist",(req,res)=>{
     // ranking()
     res.json(leaderBoardData);
@@ -15,15 +15,15 @@ app.get("/api/questions",(req,res)=>{
     res.json(data)
 });
 app.post("/api/update-data",(req,res)=>{
-    for(let i = 0 ; i<leaderBoardData.student.length;i++){
+    for(let i = 0 ; i<student.length;i++){
         // console.log(leaderBoardData.student[i].matches)
-        for(let j = 0 ; j < leaderBoardData.student[i].matches.length; j++){
+        for(let j = 0 ; j < student[i].matches.length; j++){
             // console.log(req.body.answer)
             
-            if(req.body.answer.toLowerCase() === leaderBoardData.student[i].matches[j].toLowerCase() || req.body.answer.toLowerCase() === leaderBoardData.student[i].name.toLowerCase()){
-                leaderBoardData.student[i].points[req.body.questionNo] =  leaderBoardData.student[i].points[req.body.questionNo] + 1; 
-                console.log(leaderBoardData.student[i].points[parseInt(req.body.questionNo)])
-                fileSupport.writeFile("./leaderBoard.json",JSON.stringify(leaderBoardData),(err) => {
+            if(req.body.answer.toLowerCase() === student[i].matches[j].toLowerCase() || req.body.answer.toLowerCase() === student[i].name.toLowerCase()){
+                student[i].points[req.body.questionNo] =  student[i].points[req.body.questionNo] + 1; 
+                console.log(student[i].points[parseInt(req.body.questionNo)])
+                writeFile("./leaderBoard.json",JSON.stringify(leaderBoardData),(err) => {
                     if (err)
                       console.log(err);
                     else {
